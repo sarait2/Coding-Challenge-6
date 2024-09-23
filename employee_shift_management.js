46c227b80b26c322ba000746f702dd80c5d36793
sarait2 <saraitorres999@gmail.com>
2024-09-22 21:09:31 -0400

    Task 3 - Create assignShift function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -17,6 +17,26 @@ function displayEmployeeShifts (employee) {
 }
 // Create displayEmployeeShifts function
 
+//Task 3 - Create a Function to Assign a New Shift
+function assignShift (name, day, hours) {
+        const employee = employees.find(employee => employee.name === employeeName);
+        if (!employee) {
+            console.log('Employee not found.');
+            return;
+        }
+        
+        const existingShift = employee.shifts.find(shift => shift.day === day);
+        if (existingShift) {
+            console.log(`Error: ${employeeName} is already assigned a shift on ${day}.`);
+            return;
+        }
+        
+        employee.shifts.push({ day, hours });
+}
+//Create assignShift function
+
+
+
 
     
     
